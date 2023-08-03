404ac51cd8593537f9c511ca5d559a4053022f40
ghost3344 <kashyapjivani@gmail.com>
2023-08-03 22:48:36 +0530

    fix issue with imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GlobalStyle from './styles/styledComponents';
+import {GlobalStyle} from './styles/styledComponents';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
