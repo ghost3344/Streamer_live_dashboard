830f35202fa1542ed87b91e1639c87ff00c4247d
ghost3344 <kashyapjivani@gmail.com>
2023-08-03 22:47:41 +0530

    added arrow keys

--- src/components/NumberAnimation.js
+++ src/components/NumberAnimation.js
@@ -1,51 +1,73 @@
-import { PointsCount } from '../styles/styledComponents';
-import React, { useEffect } from 'react';
-import usePrevious from '../hooks/usePrevious';
+import { PointsCount, Arrow } from "../styles/styledComponents";
+import React, { useEffect, useState } from "react";
+import usePrevious from "../hooks/usePrevious";
 
-const NumberAnimation = props => {
-  const {number ,id } = props
-  const previosNumber = usePrevious(number)
+const NumberAnimation = (props) => {
+  const { number, id } = props;
+  const previosNumber = usePrevious(number);
+  const [prevNumber,setPreNumber]=useState(0)
+  const [hide,setHide]=useState(true)
 
-const animate = (obj, initVal = 0, lastVal = 0, duration)=> {
-  let startTime = null;
+  const animate = (obj, initVal = 0, lastVal = 0, duration) => {
+    let startTime = null;
 
-//pass the current timestamp to the step function
-const step = (currentTime) => {
+    //pass the current timestamp to the step function
+    const step = (currentTime) => {
+      //if the start time is null, assign the current time to startTime
+      if (!startTime) {
+        startTime = currentTime;
+      }
 
-//if the start time is null, assign the current time to startTime
-if (!startTime) {
-  startTime = currentTime ;
-}
+      //calculate the value to be used in calculating the number to be displayed
+      const progress = Math.min((currentTime - startTime) / duration, 1);
 
-//calculate the value to be used in calculating the number to be displayed
-const progress = Math.min((currentTime - startTime)/ duration, 1);
+      //calculate what score to be displayed using the value gotten above
+      obj.innerHTML =
+        Math.floor(progress * (lastVal - initVal) + initVal) + "pts";
 
-//calculate what score to be displayed using the value gotten above
-obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal) + "pts";
+      //checking to make sure the score counter does not exceed the last value (lastVal)
+      if (progress < 1) {
+        requestAnimationFrame(step);
+      } else {
+        cancelAnimationFrame(requestAnimationFrame(step));
+      }
+    };
 
-//checking to make sure the score counter does not exceed the last value (lastVal)
-if (progress < 1) {
-  requestAnimationFrame(step);
-} else {
-     cancelAnimationFrame(requestAnimationFrame(step));
-  }
-};
+    //start animating
+    requestAnimationFrame(step);
+  };
 
-//start animating
-requestAnimationFrame(step);
+  useEffect(() => {
+    setPreNumber(previosNumber)
+    let counterId = document.getElementById(id);
+    // call animate fuction whenever score changes and update score with animation of 500ms
+    animate(counterId, previosNumber, number, 500);
+    setHide(false)
+    
+    //auto Hide arrow after fixed time
+    setTimeout(()=>{
+      setHide(true)
+    },[800])
 
-}
+  }, [number]);
 
-useEffect(()=>{
-  let counterId = document.getElementById(id);
-  // call animate fuction whenever score changes
-  animate(counterId,previosNumber, number, 500);
-},[number])
+
 
 
   return (
-      <PointsCount><span id={id}></span></PointsCount>
+    <>
+      <PointsCount>
+        <Arrow
+          $degree={prevNumber < number ? "-135deg" : "45deg"}
+          $direction={
+            (prevNumber === 0 || prevNumber === undefined) || hide ? "stable" : prevNumber < number
+              ? "up": "down"
+          }
+        ></Arrow>
+        <span id={id}></span>
+      </PointsCount>
+    </>
   );
-}
+};
 
 export default NumberAnimation;
--- src/styles/styledComponents.js
+++ src/styles/styledComponents.js
@@ -1,71 +1,88 @@
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
+export const GlobalStyle = createGlobalStyle`
+  body {
+    overflow-anchor : none;
+  }
+`;
 
 export const ListWrapper = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
 
 export const ListGroup = styled.div`
-width: 600px;
-padding: 20px;
-max-width: 100%;
-display: flex;
-flex-direction: column;
-margin: 2rem;
-overflow: hidden;
-@media (min-width: 768px) {
-      box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+  width: 600px;
+  padding: 20px;
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 2rem;
+  overflow: hidden;
+  @media (min-width: 768px) {
+    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
       0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-      border-radius: 20px;
-}   
+    border-radius: 20px;
+  }
 `;
 
 export const Flex = styled.div`
-display: flex;
-align-items: center;
-justify-content: ${props=>props?.$justify || ""};
-width: ${props=>props?.$width || ""}
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => props?.$justify || ""};
+  width: ${(props) => props?.$width || ""};
 `;
 
-
 export const StreamIndex = styled.div`
-border-radius: 50%;
-    height: 20px;
-    min-width: 20px;
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0.6rem;
-    background-color : ${props=>props?.$backgroundColor || ""};
-    p{
-        color: #ffffff;
-        font-weight: 600;
-    }
+  border-radius: 50%;
+  height: 20px;
+  min-width: 20px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.6rem;
+  background-color: ${(props) => props?.$backgroundColor || ""};
+  p {
+    color: #ffffff;
+    font-weight: 600;
+  }
 `;
 
 export const AvatarImage = styled.div`
-border-radius: 50%;
-min-height: 30px;
-min-width: 30px;
-position: relative;
-margin: 0.6rem;
+  border-radius: 50%;
+  min-height: 30px;
+  min-width: 30px;
+  position: relative;
+  margin: 0.6rem;
 `;
 
 export const Text = styled.p`
-text-align: center;
-font-weight:550;
-margin: 5px;
-font-size: 14px;
+  text-align: center;
+  font-weight: 550;
+  margin: 5px;
+  font-size: 14px;
 `;
 
 export const PointsCount = styled.p`
-text-align: center;
-font-weight: 500;
-margin: 5px;
-font-size: 14px;
-color: rgb(205, 70, 70);
+  display: flex;
+  align-items: center;
+  text-align: center;
+  font-weight: 500;
+  margin: 5px;
+  font-size: 14px;
+  color: rgb(205, 70, 70);
+`;
+
+export const Arrow = styled.p`
+  margin-right: 10px;
+  border: solid ${(props) => (props?.$direction === "down" ? "red" : "green")};
+  border-width: 0 3px 3px 0;
+  display: ${(props) => (props?.$direction === "stable" ? "none" : "inline-block")};
+  padding: 3px;
+  transform: rotate(${(props) => props?.$degree || ""});
+  -webkit-transform: rotate(${(props) => props?.$degree || ""});
 `;
